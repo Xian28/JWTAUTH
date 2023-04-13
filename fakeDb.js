112fa632fff27fbb2915d584643ff7dd7f0c0200
unknown <torres.christianleonell@gmail.com>
2023-04-13 17:02:49 +0800

    working on registering user with fake db

---
+++ fakeDb.js
@@ -0,0 +1,7 @@
+exports.fakeDb = [
+    {
+        id: 0,
+        email: 'example@email.com',
+        password: '$2a$10$kt7aCRQx4R0z6VjOH7wxZu2IAv/sRLa.f3h5.cqWAQqBp4SaHFkzS'
+    }
+]
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
+const { fakeDb } = require('./fakeDb');
 
 const server = express();
 
@@ -22,3 +23,24 @@ const port = process.env.PORT || 8080
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
+
+server.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = fakeDb.find(user => user.email === email);
+        if(user) throw new Error('User already exist');
+        const hashedPassword = await hash(password, 10);
+        fakeDb.push({
+            id: fakeDb.length,
+            email,
+            password: hashedPassword
+        });
+        res.send({message: 'User Created'});
+        console.log(fakeDb);
+        console.log(hashedPassword);
+    }   
+    catch(err){
+        console.log(err.message)
+    }
+})
