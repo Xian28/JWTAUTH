b71ac6fca560f2e19a0d19d0178c9db05bcbe21a
Christian Leonell Torres <torres.christianleonell@gmail.com>
2023-04-16 23:45:41 +0800

    now on logging in; await compare is not working even if passwords are surely correct when checked on postman

--- fakeDb.js
+++ fakeDb.js
@@ -2,6 +2,6 @@ exports.fakeDb = [
     {
         id: 0,
         email: 'example@email.com',
-        password: '$2a$10$kt7aCRQx4R0z6VjOH7wxZu2IAv/sRLa.f3h5.cqWAQqBp4SaHFkzS'
+        password: 'test'
     }
 ]
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const cors = require('cors');
 const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
 const { fakeDb } = require('./fakeDb');
+const { 
+    createAccessToken, 
+    createRefreshToken,
+    sendAccessToken,
+    sendRefreshToken
+} = require('./token.js');
 
 const server = express();
 
@@ -18,11 +24,6 @@ server.use(
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true}));
-
-const port = process.env.PORT || 8080
-server.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-});
 
 server.post('/register', async (req, res) => {
     const { email, password } = req.body;
@@ -47,3 +48,31 @@ server.post('/register', async (req, res) => {
         })
     }
 })
+
+server.post('/login', async (req ,res) => {
+    const { email, password } = req.body;
+    try{
+        const user = fakeDb.find(user => user.email === email);
+        if(!user) throw new Error('User does not exist');
+        const hashedPassword = await hash(password, 10);
+        const valid = await compare(password, user.password);
+        if(!valid) throw new Error("Password not correct" + valid + ":" + password + ":" + user.password);
+        const accesstoken =  createAccessToken(user.id);
+        const refreshtoken =  createRefreshToken(user.id);
+        user.refreshtoken = refreshtoken;
+        console.log(fakeDb);
+        sendRefreshToken(res, refreshtoken);
+        sendAccessToken(res, req, accesstoken); 
+
+    }
+    catch(err){
+        res.send({
+            error: `${err.message}`,
+        })
+    }
+})
+
+const port = process.env.PORT || 8080
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
