1dceb53c82d23356cb174956316eca627202a7cc
unknown <torres.christianleonell@gmail.com>
2023-04-20 09:39:28 +0800

    complete jwt app

--- frontend/src/components/Protected.js
+++ frontend/src/components/Protected.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserContext } from '../App'
 
 const Protected = () => {
-    return (
-        <div>Protected</div>
-    )
+    const [user] = useContext(UserContext);
+    const [content, setContent] = useState('You need to login');
+
+    useEffect(() => {
+        async function fetchProtected(){
+            const result = await (await fetch('http://localhost:4000/protected', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: `Bearer ${user.accesstoken}`,
+                }
+            })).json();
+            if(result.data) setContent(result.data);
+        }
+        fetchProtected();
+    },[user])
+
+    return <div>
+        {content}
+    </div>
+    
 }
 
 export default Protected
